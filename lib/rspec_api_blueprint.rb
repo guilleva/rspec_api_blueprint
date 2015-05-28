b390f92c5e9451eb472b2c2bf17922515ca94b0e
Grayden Smith <grayden@tech-vision.ca>
2015-05-28 10:44:18 -0400

    Refactor blueprint writing into methods

--- lib/rspec_api_blueprint.rb
+++ lib/rspec_api_blueprint.rb
@@ -1,12 +1,6 @@
 require "rspec_api_blueprint/version"
 require "rspec_api_blueprint/string_extensions"
 
-def api_docs_folder_path
-  return File.join(Rails.root, '/docs/', '/api_docs/') if defined? Rails
-
-  File.join(File.expand_path('.'), '/api_docs/')
-end
-
 RSpec.configure do |config|
   config.before(:suite) do
     Dir.mkdir(api_docs_folder_path) unless Dir.exists?(api_docs_folder_path)
@@ -40,103 +34,116 @@ RSpec.configure do |config|
     next unless example.metadata[:document] === true
 
     if response
-      example_group = example.example_group.metadata
-      example_groups = []
+      action_group = example.example_group.metadata
+      resource_group = action_group[:parent_example_group]
+      grouping_group = resource_group[:parent_example_group]
 
-      while example_group
-        example_groups << example_group
-        example_group = example_group[:parent_example_group]
-      end
-
-      # example_groups[0] (Action) -> Check and employee out of a seat [DELETE]
-      # example_groups[1] (Resource) -> Check ins [/api/1/check_ins]
-      # example_groups[2] (Resource group name) -> Check In requests
+      next if action_group.nil? || resource_group.nil? || grouping_group.nil?
 
       # #   Group Name
       # ##  Collection [/collection]
       # ### Action Name [GET]
 
-      example_groups[2][:description_args].first.match(/(.+)\sRequests/)
+      grouping_group[:description_args].first.match(/(.+)\sRequests/)
       file_name = $1.gsub(' ','').underscore
 
-      example_groups[1][:description_args].first.match(/(.+)\[(.+)\]/)
+      resource_group[:description_args].first.match(/(.+)\[(.+)\]/)
       resource_description = $1
       resource = $2
 
-      example_groups[0][:description_args].first.match(/(.+)\[(.+)\]/)
+      action_group[:description_args].first.match(/(.+)\[(.+)\]/)
       action_description = $1
       action = $2.upcase
 
       file = "#{api_docs_folder_path}#{file_name}_blueprint.md"
 
       File.open(file, 'a') do |f|
-        # Resource & Action
-        query_strings = URI.decode(request.env['QUERY_STRING']).split('&')
-        params = query_strings.map do |value|
-          value.gsub("[","%5B").gsub("]","%5D")
-        end
-
-        unless File.readlines(file).grep(%r{#{resource}}).any?
-          f.write "## #{resource_description} [#{resource}]" 
-          f.write("\n")
-        end
-        f.write "### #{action_description} [#{action}]"
-
-        unless params.empty?
-         f.write "?#{params.join('&')}"
-        end
-
-        f.write("\n")
-
-        # Request
-        request_body = request.body.read
-
-        current_env  = request.env ? request.env : request.headers
-
-        authorization_header = current_env['HTTP_AUTHORIZATION']   ||
-          env['X-HTTP_AUTHORIZATION'] ||
-          env['X_HTTP_AUTHORIZATION'] ||
-          env['REDIRECT_X_HTTP_AUTHORIZATION'] ||
-          env['AUTHORIZATION']
-
-
-        if request_body.present? || authorization_header.present? || request.env['QUERY_STRING']
-          f.write "+ Request #{request.content_type}\n\n"
-
-          if request.env['QUERY_STRING'].present?
-            f.write "+ Parameters\n\n".indent(4)
-            query_strings = URI.decode(request.env['QUERY_STRING']).split('&')
-
-            query_strings.each do |value|
-              key, example = value.split('=')
-              f.write "+ #{key} = '#{example}'\n".indent(12)
-            end
-            f.write("\n")
-          end
-
-          allowed_headers = %w(HTTP_AUTHORIZATION AUTHORIZATION CONTENT_TYPE)
-          f.write "+ Headers\n\n".indent(4)
-          current_env.each do |header, value|
-            next unless allowed_headers.include?(header)
-            header = header.gsub(/HTTP_/, '') if header == 'HTTP_AUTHORIZATION'
-            f.write "#{header}: #{value}\n".indent(12)
-          end
-          f.write "\n"
-
-          # Request Body
-          if request_body.present? && request.content_type.to_s == 'application/json'
-            f.write "+ Body\n\n".indent(4) if authorization_header
-            f.write "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header ? 12 : 8)
-          end
-        end
-
-        # Response
-        f.write "+ Response #{response.status} (#{response.content_type}; charset=#{response.charset})\n\n"
-
-        if response.body.present? && response.content_type.to_s =~ /application\/json/
-          f.write "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(8)
-        end
+        write_resource_if_first(f, file, resource, resource_description)
+        write_action(f, action, action_description)
+        write_request(f)
+        write_response(f)
       end unless response.status == 401 || response.status == 403 || response.status == 301
     end
   end
 end
+
+def api_docs_folder_path
+  return File.join(Rails.root, '/docs/', '/api_docs/') if defined? Rails
+
+  File.join(File.expand_path('.'), '/api_docs/')
+end
+
+def write_resource_if_first(f, file, resource, resource_description)
+  unless File.readlines(file).grep(%r{#{resource}}).any?
+    f.write "## #{resource_description} [#{resource}]"
+    f.write("\n")
+  end
+end
+
+def write_action(f, action, action_description)
+  f.write "### #{action_description} [#{action}]"
+
+  query_strings = URI.decode(request.env['QUERY_STRING']).split('&')
+
+  params = query_strings.map do |value|
+    value.gsub("[","%5B").gsub("]","%5D")
+  end
+
+  unless params.empty?
+   f.write "?#{params.join('&')}"
+  end
+
+  f.write("\n")
+end
+
+def write_request(f)
+  request_body = request.body.read
+
+  current_env  = request.env ? request.env : request.headers
+
+  authorization_header = current_env['HTTP_AUTHORIZATION']   ||
+    env['X-HTTP_AUTHORIZATION'] ||
+    env['X_HTTP_AUTHORIZATION'] ||
+    env['REDIRECT_X_HTTP_AUTHORIZATION'] ||
+    env['AUTHORIZATION']
+
+
+  if request_body.present? || authorization_header.present? || request.env['QUERY_STRING']
+    f.write "+ Request #{request.content_type}\n\n"
+
+    if request.env['QUERY_STRING'].present?
+      f.write "+ Parameters\n\n".indent(4)
+      query_strings = URI.decode(request.env['QUERY_STRING']).split('&')
+
+      query_strings.each do |value|
+        key, example = value.split('=')
+        f.write "+ #{key} = '#{example}'\n".indent(12)
+      end
+      f.write("\n")
+    end
+
+    allowed_headers = %w(HTTP_AUTHORIZATION AUTHORIZATION CONTENT_TYPE)
+    f.write "+ Headers\n\n".indent(4)
+    current_env.each do |header, value|
+      next unless allowed_headers.include?(header)
+      header = header.gsub(/HTTP_/, '') if header == 'HTTP_AUTHORIZATION'
+      f.write "#{header}: #{value}\n".indent(12)
+    end
+    f.write "\n"
+
+    # Request Body
+    if request_body.present? && request.content_type.to_s == 'application/json'
+      f.write "+ Body\n\n".indent(4) if authorization_header
+      f.write "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header ? 12 : 8)
+    end
+  end
+end
+
+def write_response(f)
+  f.write "+ Response #{response.status} (#{response.content_type}; charset=#{response.charset})\n\n"
+
+  if response.body.present? && response.content_type.to_s =~ /application\/json/
+    f.write "#{JSON.pretty_generate(JSON.parse(response.body))}\n\n".indent(8)
+  end
+end
+
